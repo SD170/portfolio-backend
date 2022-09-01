83eb2c35540620402b4b3579ce5bd649fe3e37c3
SD170 <duttasaswata7@gmail.com>
2022-09-01 20:41:26 +0530

    project model added

---
+++ src/models/Project.model.ts
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const ProjectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: [true, "Please add a course title"],
+    },
+    githubLink: {
+        type: String,
+    },
+    outline: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    startDate: {
+        type: Date,
+    },
+    endDate: {
+        type: Date,
+    },
+    pictureUrls: {
+        type: [String], // will be saved in s3
+    },
+    fileUrls: {
+        type: [String], // will be saved in s3
+    },
+    tags: {
+        type: [String],
+    },
+    developers: {
+        type: [String],
+    },
+    otherLinks: {
+        type: [String],
+    },
+    achievements: {
+        type: [String],
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+
+module.exports = mongoose.model("Project", ProjectSchema);
