0553774d76f99bb860e82765d40ad96a57694a4a
SD170 <duttasaswata7@gmail.com>
2022-09-03 15:39:42 +0530

    routes outline added

---
+++ src/routes/project.route.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+// import { prefetchProject, createProject, getProjects, updateProject, deleteProject} from '../controllers/project.controller';
+
+
+const router = Router();
+
+
+
+// todo routes:
+
+// router.route("/").get(getProjects);
+// router.route("/preFetch").get(prefetchProject);
+// router.route("/create").post(createProject);
+// router.route("/update/:id").put(updateProject);
+// router.route("/delete/:id").delete(deleteProject);
+
+export default router;
