91e8071a85494e7a46df1c8a4475be25ede6dbde
SD170 <duttasaswata7@gmail.com>
2022-09-13 11:04:47 +0530

    errorresponse test

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { Request, Response, NextFunction } from 'express';
 import ProjectModel from "../models/Project.model";
 import asyncHandler from "../middlewares/async";
+import ErrorResponse from '../utils/ErrorResponse';
 
 
 
@@ -9,7 +10,7 @@ import asyncHandler from "../middlewares/async";
 //  @desc       get projects
 //  @route      GET /api/v1/projects/
 //  @access     Public
-export const getProjects = asyncHandler(async (req: Request, res: Response) => {
+export const getProjects = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
    
     const projects = await ProjectModel.find({ ...req.query });
 
@@ -25,7 +26,11 @@ export const getProjects = asyncHandler(async (req: Request, res: Response) => {
 //  @desc       get single project
 //  @route      GET /api/v1/projects/:id
 //  @access     Public
-export const getProject = asyncHandler(async (req: Request, res: Response) => {
+export const getProject = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+
+    if (!req.params.id) {
+        next(new ErrorResponse("Please provide projectId in path params", 400))
+    }
    
     const project = await ProjectModel.findById(req.params.id);
 
