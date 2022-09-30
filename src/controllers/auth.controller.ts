3709b251589654c164dc96b63447b5683a0e371b
SD170 <duttasaswata7@gmail.com>
2022-09-30 23:57:23 +0530

    discord oauth2 token call

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { Request, Response, NextFunction } from 'express';
+import axios from "axios";
 import ProjectModel from "../models/Project.model";
 import asyncHandler from "../middlewares/async";
 import ErrorResponse from '../utils/ErrorResponse';
@@ -11,7 +12,7 @@ import ErrorResponse from '../utils/ErrorResponse';
 /**
  * after successful redirect we'll get a code in the query params. (/api/v1/auth/discord/redirect?code=ctlhzwhaCSANPKfIoQSkhdInGiJZze)
  * With the code we can ask for auth token from discord, with that auth token, we can ask for info on the user.
- * The auth token also expires, we need to refresh it time and again.
+ * The auth token also expires, we need to refresh it after some time.
  */
 
 
@@ -19,14 +20,45 @@ import ErrorResponse from '../utils/ErrorResponse';
 //  @route      GET /api/v1/auth/discord/redirect
 //  @access     Public
 export const discordRedirect = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, DISCORD_OAUTH_TOKEN_URL, SERVER_URL } = process.env;
+    const discordAuthTokenUrl = DISCORD_OAUTH_TOKEN_URL || "https://discord.com/api/v10/oauth2/token";
 
-    // const projects = await ProjectModel.find({ ...req.query });
+    const { code } = req.query;
+    if (code) {
+        // means this api call is done from the discord oauth2 link
+        
+        // next(new ErrorResponse("code not found in the query, from discord authentication", 400));
+
+        // const projects = await ProjectModel.find({ ...req.query });
+
+        // we'll redirect to the same api handler, but then the code will not be present (we can put the condition on that)
+        // Auth token will be present then.
+        const redirectUrl = `${SERVER_URL}/api/v1/auth/discord/redirect` 
+
+
+
+        // making a post req to the discord oauth TOKEN URL, with the code. Will get the access token in return.
+        const result = await axios.post(discordAuthTokenUrl, {
+            'client_id': DISCORD_CLIENT_ID,
+            'client_secret': DISCORD_CLIENT_SECRET,
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': redirectUrl
+        }, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        console.log(result);
+    }
+
 
     res.status(200)
         .json({
             success: true,
             message: `you're redirected`,
-            data: {},
+            data: { code },
         });
 
 });
